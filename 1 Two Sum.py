bbd308a2034235b9dd9e45e83db2782d79da4dc6
Yang <sheep-sheep@users.noreply.github.com>
2016-09-06 00:02:49 -0400

    Use hashmap to achieve avg O(n) solution

--- 1 Two Sum.py
+++ 1 Two Sum.py
@@ -11,3 +11,25 @@ class Solution(object):
             for j in nums[count:]:
                 if i + j == target:
                     return [nums.index(i), nums[count:].index(j)+count]
+
+    def twoSum_hash(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        searchdict = {}
+        count = 0
+        for i in nums:
+            searchdict[i] = count
+            count = count + 1
+        count = 0
+        for i in nums:
+            num = target - i
+            try:
+                index = searchdict[num]
+                if index != count:
+                    return [count, index]
+            except KeyError:
+                pass
+            count = count + 1
