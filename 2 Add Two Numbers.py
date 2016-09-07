ab41d2622d34589b56b1d167fd5d617607f4fbd7
Yang <sheep-sheep@users.noreply.github.com>
2016-09-06 22:29:23 -0400

    Solution 2: 82.68%
    This time use a little trick to handle the calculation part, the idea is lazy and it may not be efficient enough.

--- 2 Add Two Numbers.py
+++ 2 Add Two Numbers.py
@@ -65,3 +65,46 @@ class Solution(object):
         return result
         
         
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution_2(object):
+    
+    def convertLLtoList(self, L):
+        tmp = []
+        while(L.val != None):
+            tmp.append(L.val)
+            if L.next != None:
+                L = L.next
+            else:
+                break
+        return tmp
+        
+    def convertListtoNum(self, nums):
+        count = 0
+        result = 0
+        for num in nums:
+            result = result + num * 10**count
+            count = count + 1
+        return result
+        
+    def creatLLfromList(self, l):
+        if len(l) == 1:
+            return ListNode(l[0])
+        else:
+            node = ListNode(l[-1])
+            node.next = self.creatLLfromList(l[:-1])
+            return node
+
+    def addTwoNumbers(self, l1, l2):
+        l1_num = self.convertListtoNum(self.convertLLtoList(l1))
+        l2_num = self.convertListtoNum(self.convertLLtoList(l2))
+        result = l1_num + l2_num 
+        result_list = [int(i) for i in list(str(result))]
+        return self.creatLLfromList(result_list)
+        
+        
