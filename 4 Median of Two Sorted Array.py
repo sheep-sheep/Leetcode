c947fa1596ea819f3744ad15e112aa3e27ba9eaf
Yang <sheep-sheep@users.noreply.github.com>
2017-02-08 22:56:58 -0500

    The standard solution

--- 4 Median of Two Sorted Array.py
+++ 4 Median of Two Sorted Array.py
@@ -1,4 +1,47 @@
 class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m = len(nums1)
+        n = len(nums2)
+        # do the switch
+        if m > n:
+            nums1, nums2, m, n = nums2, nums1, n, m
+        lo = 0
+        hi = m
+        half = (m+n+1)/2
+
+        while(lo <= hi):
+            i = (lo + hi)/2
+            j = half - i
+            if i < m and nums2[j - 1] > nums1[i]:
+                lo = i + 1
+            elif i > 0 and nums1[i - 1] > nums2[j]:
+                hi = i - 1
+            else:
+                if i == 0:
+                    maxofleft = nums2[j-1]
+                elif j == 0:
+                    maxofleft = nums1[i-1]
+                else:
+                    maxofleft = max(nums1[i-1], nums2[j-1])
+
+                if (m + n) % 2 == 1:
+                    return maxofleft
+
+                if i == m:
+                    minofright = nums2[j]
+                elif j == n:
+                    minofright = nums1[i]
+                else:
+                    minofright = min(nums1[i], nums2[j])
+
+                return (maxofleft + minofright)/2.0
+
+class Solution_1(object):
 
     def findMedianSortedArrays(self, nums1, nums2):
         """
