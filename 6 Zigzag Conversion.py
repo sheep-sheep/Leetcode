e6bedafa75abd7f0c00a0f662879d123a523a724
Yang <sheep-sheep@users.noreply.github.com>
2017-02-10 22:22:30 -0500

    Update 6 Zigzag Conversion.py
    I should read the description more carefully, make k as an pointer to print each char, and the k will move from bottom to top back and fourth.

--- 6 Zigzag Conversion.py
+++ 6 Zigzag Conversion.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        # edgecase:
         if len(s) == 0:
             return ''
         if numRows == 1:
@@ -13,19 +14,19 @@ class Solution(object):
         for i in range(numRows):
             zigzag[i] = ''
         i, k, holder= 0, 0, 0
+        updirection = True
         while(i < len(s)):
-            if i!=0 and (i-holder)%numRows == 0:
-                if numRows !=2:
-                    zigzag[numRows/2] += s[i]
-                    holder += 1
-                    i += 1
-                k = 0
-                if i == len(s):
-                    break
             # case0: fill 1st column
             zigzag[k] += s[i]
+            if k == (numRows - 1):
+                updirection = False
+            elif k == 0 and not updirection:
+                updirection = True
+            if updirection:
+                k += 1
+            else:
+                k -= 1
             i += 1
-            k += 1
         output = ''
         for key in zigzag.keys():
             output += zigzag[key]
