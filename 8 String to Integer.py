1b40f7c2c0d11cadaafbf076bb71a473e6318642
Yang <sheep-sheep@users.noreply.github.com>
2017-02-14 21:05:31 -0500

    Create 8 String to Integer.py

---
+++ 8 String to Integer.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        # remove the leading spaces and the spaces in the middle
+        str = str.strip(' ')
+        str = str.replace(' ', '')
+
+        # deal with the positive and negative sign
+        signed = False
+        if str.startswith('-'):
+            signed = True
+            str = str.replace('-', '')
+        elif str.startswith('+'):
+            signed = False
+            str = str.replace('+', '')
+        else:
+            signed = False
+
+        num = 0
+        # check the invalid symbols & INTMAX
+        for char in str:
+            if char <= '9' and char >='0':
+                num = num*10 + int(char)
+                if signed and num >= 1<<31:
+                    num = 1<<31
+                    break
+                elif not signed and num >= 1<<31 - 1:
+                    num = (1<<31) - 1
+                    break
+            else:
+                break
+        if signed:
+            num = -num
+        return num
