87fd47c34ad3cc90bd7f6a574d1e5a7f9b06b5ed
Yang <sheep-sheep@users.noreply.github.com>
2017-02-15 23:03:44 -0500

    Update 11 Container With Most Water.py

--- 11 Container With Most Water.py
+++ 11 Container With Most Water.py
@@ -9,3 +9,12 @@ class Solution:
             else:
                 j -= 1
         return water
+
+# Nice explaination:
+      1 2 3 4 5 6
+1 x ------- o
+2 x x - o o o
+3 x x x o | |
+4 x x x x | |
+5 x x x x x |
+6 x x x x x x
