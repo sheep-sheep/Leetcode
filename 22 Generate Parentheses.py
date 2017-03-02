732018fb3a210e515a747316aa32815c13eb82a8
Yang <sheep-sheep@users.noreply.github.com>
2017-03-01 22:43:58 -0500

    #TODO: learn recursive way

--- 22 Generate Parentheses.py
+++ 22 Generate Parentheses.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+
+        def generate(p, left, right, parens=[]):
+            if left:
+                generate(p + '(', left - 1, right)
+            if right > left:
+                generate(p + ')', left, right - 1)
+            if not right:
+                parens += p,
+            return parens
+
+        return generate('', n, n)
+
+class Solution_0(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
         if n == 1:
             return ['()']
         elif n == 2:
