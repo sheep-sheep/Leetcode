fb48795e25da07e17f0fde867d0520d9be25d389
Yang <sheep-sheep@users.noreply.github.com>
2017-03-06 00:01:03 -0500

    Update 27 Remove Element.py

--- 27 Remove Element.py
+++ 27 Remove Element.py
@@ -6,3 +6,18 @@
         else:
             start +=1
     return start
+
+  
+  class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        head = 0
+        for i in range(len(nums)):
+            if(nums[i] != val):
+                nums[head] = nums[i]
+                head += 1
+        return head
