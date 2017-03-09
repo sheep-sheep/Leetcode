70eadebb724f3e21e642b5bc449ea11c1a9d64be
Yang <sheep-sheep@users.noreply.github.com>
2017-03-08 23:03:40 -0500

    Update 31 Next Permutation.py

--- 31 Next Permutation.py
+++ 31 Next Permutation.py
@@ -6,6 +6,37 @@ class Solution(object):
         """
         if not nums or len(nums) == 1:
             return
+        for i in range(len(nums)-1, -1, -1):
+            right = i
+            left = i -1
+            if left == 0 and nums[left] > nums[right]:
+                nums.sort()
+                return
+            if nums[left] < nums[right]:
+                break
+        right = 0
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] > nums[left]:
+                right = i
+                break
+        nums[left], nums[right] = nums[right], nums[left]
+        self.reverse(nums, left + 1, len(nums) - 1)
+
+    def reverse(self,nums,l,r):
+        while l < r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
+# The part I missed was to reverse the array between left and the end.
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums or len(nums) == 1:
+            return
         def nextPermu(left, right, nums, minNumIndex):
             if left == 0:
                 if nums[left] > nums[minNumIndex] and nums[left] >= nums[right]:
