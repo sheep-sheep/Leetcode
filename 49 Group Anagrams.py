ff253b0e40b7887e552afbf083bc577e80fce6fb
Yang <sheep-sheep@users.noreply.github.com>
2017-03-12 17:49:29 -0400

    Update 49 Group Anagrams.py

--- 49 Group Anagrams.py
+++ 49 Group Anagrams.py
@@ -1,3 +1,21 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        result = {}
+        for str in strs:
+            group = ''
+            for i in sorted(str):
+                group+=i
+            if group not in result:
+                result[group] = [str]
+            else:
+                result[group] += [str]
+        return result.values()
+# This is my initial solution, need to understand the difference between tuple and list, so the tuple can be hashable?!
+
 def groupAnagrams(self, strs):
     groups = collections.defaultdict(list)
     for s in strs:
