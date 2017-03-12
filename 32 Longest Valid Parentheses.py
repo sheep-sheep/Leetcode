7ae3fd1b509838576a686e7d0d9245d67327b439
Yang <sheep-sheep@users.noreply.github.com>
2017-03-12 18:59:43 -0400

    Update 32 Longest Valid Parentheses.py

--- 32 Longest Valid Parentheses.py
+++ 32 Longest Valid Parentheses.py
@@ -1,27 +1,43 @@
 class Solution(object):
     def longestValidParentheses(self, s):
+         stack = [0]
+         curr_max = 0
+         s = ')'+s # create a invalid case for 0 index
+         for i in range(1, len(s)):
+             if s[i] == ')' and s[stack[-1]] == '(':
+                 stack.pop()
+                 curr_max = max(curr_max, i - stack[-1])
+             else:
+                 stack.append(i)
+         return curr_max
+
+
+# Time Limit Exceeded!!!    But my solution has the potential to print the longest string!
+class Solution(object):
+    def longestValidParentheses(self, s):
         """
         :type s: str
         :rtype: int
         """
-        # use 1D DP
-        # dp[i] records the longestValidParenthese EXACTLY ENDING at s[i]
-        dp = [0 for x in xrange(len(s))]
-        max_to_now = 0
-        for i in xrange(1,len(s)):
-            if s[i] == ')':
-                # case 1: ()()
-                if s[i-1] == '(':
-                    # add nearest parentheses pairs + 2
-                    dp[i] = dp[i-2] + 2
-                # case 2: (()) 
-                # i-dp[i-1]-1 is the index of last "(" not paired until this ")"
-                elif i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == '(':
-                    if dp[i-1] > 0: # content within current matching pair is valid 
-                    # add nearest parentheses pairs + 2 + parentheses before last "("
-                        dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
-                    else:
-                    # otherwise is 0
-                        dp[i] = 0
-                max_to_now = max(max_to_now, dp[i])
-        return max_to_now
+        pq = {}
+        i = 0
+        if len(s) <2:
+            return 0
+        for parenth in s:
+            if parenth == '(':
+                pq[i] = '('
+            else:
+                if pq and pq[max(pq.keys())] == '(':
+                    pq.pop(max(pq.keys()), None)
+                else:
+                    pq[i] = ')'
+            i += 1
+        result = []
+        invalid = sorted(pq.keys())
+        if invalid:
+            for i in range(len(invalid)):
+                if i != 0:
+                    result +=[invalid[i] - invalid[i-1] - 1]
+            return max(invalid[0], max(result) if result else 0, len(s) - 1 - invalid[-1])
+        else:
+            return len(s)
