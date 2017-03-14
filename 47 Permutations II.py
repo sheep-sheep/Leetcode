d17c8c7f73f805f4e335d6366df23d8fcbf569c7
Yang <sheep-sheep@users.noreply.github.com>
2017-03-13 22:52:11 -0400

    Update 47 Permutations II.py

--- 47 Permutations II.py
+++ 47 Permutations II.py
@@ -1,10 +1,23 @@
-def permuteUnique(self, nums):
-    ans = [[]]
-    for n in nums:
-        new_ans = []
-        for l in ans:
-            for i in xrange(len(l)+1):
-                new_ans.append(l[:i]+[n]+l[i:])
-                if i<len(l) and l[i]==n: break              #handles duplication
-        ans = new_ans
-    return ans
+
+
+
+# 1st solution, i know the check part will need lots of time.
+import copy
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def recursivePermute(nums, lo, result):
+            if lo >= len(nums)-1:
+                if nums not in result:
+                    result.append(copy.deepcopy(nums))
+                return
+            for i in range(lo, len(nums)):
+                nums[i], nums[lo] = nums[lo], nums[i]
+                recursivePermute(nums, lo+1, result)
+                nums[i], nums[lo] = nums[lo], nums[i]
+        result = []
+        recursivePermute(nums, 0, result)
+        return result
