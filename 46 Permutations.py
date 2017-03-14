2649941755a735acce0ce0246c1bd98eec785578
Yang <sheep-sheep@users.noreply.github.com>
2017-03-13 22:26:28 -0400

    Update 46 Permutations.py

--- 46 Permutations.py
+++ 46 Permutations.py
@@ -1,3 +1,24 @@
+import copy
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def recursivePermute(nums, lo, result):
+            if lo >= len(nums)-1:
+                result.append(copy.deepcopy(nums))
+                return
+
+            for i in range(lo, len(nums)):
+                nums[i], nums[lo] = nums[lo], nums[i]
+                recursivePermute(nums, lo+1, result)
+                nums[i], nums[lo] = nums[lo], nums[i]
+        result = []
+        recursivePermute(nums, 0, result)
+        return result
+#Recursively shuffle the list to get the new permutation.
+
 #Failed solution
 import copy
 class Solution(object):
