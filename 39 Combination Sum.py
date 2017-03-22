6756a92dc6bc5dbd8e4fa3a4c7b2e0fd875d7314
Yang <sheep-sheep@users.noreply.github.com>
2017-03-21 22:15:24 -0400

    #TODO: summarize

--- 39 Combination Sum.py
+++ 39 Combination Sum.py
@@ -1,22 +1,22 @@
-class Solution {
-public:
-    std::vector<std::vector<int> > combinationSum(std::vector<int> &candidates, int target) {
-        std::sort(candidates.begin(), candidates.end());
-        std::vector<std::vector<int> > res;
-        std::vector<int> combination;
-        combinationSum(candidates, target, res, combination, 0);
-        return res;
-    }
-private:
-    void combinationSum(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
-        if (!target) {
-            res.push_back(combination);
-            return;
-        }
-        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
-            combination.push_back(candidates[i]);
-            combinationSum(candidates, target - candidates[i], res, combination, i);
-            combination.pop_back();
-        }
-    }
-};
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def findSum(candidates, target, idx, path, res):
+            if target < 0:
+               return []
+            elif target == 0:
+                res.append(path)
+
+            for i in range(idx, len(candidates)):
+                findSum(candidates, target-candidates[i], i, path + [candidates[i]], res)
+        res = []
+        if not candidates:
+            return []
+        else:
+            candidates.sort()
+            findSum(candidates, target, 0, [], res)
+            return res
