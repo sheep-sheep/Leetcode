7676c2a6b5f7d3798a2ae196a414e519830830ad
Yang <sheep-sheep@users.noreply.github.com>
2017-03-21 22:44:48 -0400

    #TODO: still confusing

--- 38 Count and Say.py
+++ 38 Count and Say.py
@@ -1,8 +1,21 @@
 class Solution(object):
-    # Solution 1 ... using a regular expression
     def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
         s = '1'
-        for _ in range(n - 1):
-            s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        for i in range(n-1):
+            count = 1
+            temp = []
+            for index in range(1, len(s)):
+                if s[index] == s[index-1]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
         return s
-        
