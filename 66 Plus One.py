5dc12ae2c19faed5f0a752323902571ff6eff31e
Yang <sheep-sheep@users.noreply.github.com>
2017-03-30 23:10:56 -0400

    #TODO: digit conversion

---
+++ 66 Plus One.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        return [int(char) for char in str(int(''.join([str(digit) for digit in digits]))+1)]
