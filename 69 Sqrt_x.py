7d00be4e0378ed0b9ca8fb6de8b2fde5c6ee42ed
Yang <sheep-sheep@users.noreply.github.com>
2017-04-05 23:13:40 -0400

    Need to handle Int overflow

--- 69 Sqrt_x.py
+++ 69 Sqrt_x.py
@@ -9,3 +9,22 @@ class Solution(object):
         while r*r > x:
             r = (r + x/r) / 2
         return r
+
+    
+# Binary Search
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        right = x
+        left = 1
+        while(True):
+            mid = left + (right - left)/2
+            if mid*mid>x:
+                right =mid - 1
+            elif mid*mid == x:
+                return mid
+            else:
+                left = mid + 1
