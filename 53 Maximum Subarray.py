4740c3c0669bf493bfb261ffa1416f63ae96131d
Yang <sheep-sheep@users.noreply.github.com>
2017-04-06 23:18:15 -0400

    Update 53 Maximum Subarray.py

--- 53 Maximum Subarray.py
+++ 53 Maximum Subarray.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res = []
-        sum = 0
-        for num in nums:
-            if sum + num > sum:
-                sum += num
-            else:
-                sum = 0
-                res += [sum]
-        return max(res)        
+        if not nums:
+            return 0
+
+        curSum = maxSum = nums[0]
+        for num in nums[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
+
+# we don't need to use DP or this is already a DP solution.    
         
