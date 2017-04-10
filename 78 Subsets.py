87ee24d873a0f93aa3f11e7c3cfba69b861fcb20
Yang <sheep-sheep@users.noreply.github.com>
2017-04-09 22:43:10 -0400

    #TODO: learn backtrack

--- 78 Subsets.py
+++ 78 Subsets.py
@@ -1,3 +1,17 @@
-#TODO:
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            return []
+        res = []
+        nums.sort()
+        self.backTrack(nums, 0, [], res)
+        return res
 
-this is a placeholder for Saturday. Social Night
+    def backTrack(self, nums, pos, path, res):
+        res.append(path)
+        for i in range(pos, len(nums)):
+            self.backTrack(nums, i+1, path +[nums[i]], res)
