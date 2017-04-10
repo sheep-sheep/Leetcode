c50a67e9e342ad6b8260a0b852aa7aaffb676842
Yang <sheep-sheep@users.noreply.github.com>
2017-04-09 23:24:55 -0400

    Careful about different cases!

--- 79 Word Search.py
+++ 79 Word Search.py
@@ -1,5 +1,29 @@
-#TODO：
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if not board:
+            return False
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if self.dfs(board, row, col, word):
+                    return True
+        return False
 
-Place holder for Saturday Night!!!!
+    def dfs(self, board, row, col, word):
+        # find word starting at (row, col)
+        if len(word) == 0: # nothing to check within the word
+            return True
+        if row<0 or row >=len(board) or col < 0 or col >= len(board[0]) or word[0]!=board[row][col]:
+            return False
 
-Step out of the comfort zone!
+        tmp = board[row][col]
+        board[row][col] = "#"  # avoid visit agian 
+        res = self.dfs(board, row+1, col, word[1:]) or self.dfs(board, row-1, col, word[1:])\
+                or self.dfs(board, row, col+1, word[1:]) or self.dfs(board, row, col-1, word[1:])
+
+        board[row][col] = tmp # assign the value back to remove the mark
+        return res
