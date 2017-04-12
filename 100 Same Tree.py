f0101ae835c5526702b9595361ca4ca0a2889766
Yang <sheep-sheep@users.noreply.github.com>
2017-04-11 23:58:04 -0400

    Update 100 Same Tree.py

--- 100 Same Tree.py
+++ 100 Same Tree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def isSameTree(self, p, q):
@@ -12,24 +12,29 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        pass
+        def preOrder(root, res):
+            res.append(root.val)
+            if root.left:
+                preOrder(root.left, res)
+            if root.right:
+                preOrder(root.right, res)
+    
+        res1, res2 = [], []
+        preOrder(p, res1)
+        preOrder(q, res2)
+        
+        if len(res1)!= len(res2):
+            return False
+        elif (not p) and (not q):
+            return True
+        else:
+            print p 
+            print q
+            count = 0
+            for res in res1:
+                if res != res2[count]:
+                    return False
+            count +=1
+        return True
 
-
-root = TreeNode(0)
-root.left = TreeNode(1)
-root.right = TreeNode(2)
-root.left.left = TreeNode(3)
-root.left.right = TreeNode(4)
-root.right.left = TreeNode(5)
-root.right.right = TreeNode(6)
-
-def dfs(root, res):
-    res.append(root.val)
-    if root.left:
-        dfs(root.left, res)
-    elif root.right:
-        dfs(root.right, res)
-
-res = []
-dfs(root, res)
-print res
+                
