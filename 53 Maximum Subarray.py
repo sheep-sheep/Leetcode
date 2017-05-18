a2a9d647b5dd75569f13de5bfdbb1e49f4debb19
Yang <sheep-sheep@users.noreply.github.com>
2017-05-18 00:04:29 -0400

    Update 53 Maximum Subarray.py

--- 53 Maximum Subarray.py
+++ 53 Maximum Subarray.py
@@ -29,3 +29,21 @@ class Solution(object):
             globalMax = max(endingMax, globalMax)
 
         return globalMax
+
+    
+# with indexes:
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        globalMax = [(0, 1)]*len(nums)
+        endingMax = nums[0]
+        sumMax = nums[0]
+        for i in range(1, len(nums)):
+            temp = endingMax+nums[i]
+            endingMax = max(nums[i], temp)
+            globalMax[i] = (i, i+1) if temp < nums[i] else (globalMax[i-1][0], globalMax[i-1][1]+1)
+            sumMax = max(endingMax, sumMax)
+        return nums[globalMax[-1][0]:globalMax[-1][1]]
