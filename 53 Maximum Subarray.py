4096b5eec672e3d8d947d64c8f679bbf6b39bbf2
Yang <sheep-sheep@users.noreply.github.com>
2017-05-17 23:26:36 -0400

    Update 53 Maximum Subarray.py

--- 53 Maximum Subarray.py
+++ 53 Maximum Subarray.py
@@ -15,21 +15,17 @@ class Solution(object):
         return maxSum
 
 # we don't need to use DP or this is already a DP solution.    
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        globalMax = nums[0]
+        endingMax = nums[0]
 
-this problem was discussed by Jon Bentley (Sep. 1984 Vol. 27 No. 9 Communications of the ACM P885)
+        for i in range(1, len(nums)):
+            endingMax = max(nums[i], endingMax+nums[i])
+            globalMax = max(endingMax, globalMax)
 
-the paragraph below was copied from his paper (with a little modifications)
-
-algorithm that operates on arrays: it starts at the left end (element A[1]) and scans through to the right end (element A[n]), keeping track of the maximum sum subvector seen so far. The maximum is initially A[0]. Suppose we've solved the problem for A[1 .. i - 1]; how can we extend that to A[1 .. i]? The maximum
-sum in the first I elements is either the maximum sum in the first i - 1 elements (which we'll call MaxSoFar), or it is that of a subvector that ends in position i (which we'll call MaxEndingHere).
-
-MaxEndingHere is either A[i] plus the previous MaxEndingHere, or just A[i], whichever is larger.
-
-public static int maxSubArray(int[] A) {
-    int maxSoFar=A[0], maxEndingHere=A[0];
-    for (int i=1;i<A.length;++i){
-    	maxEndingHere= Math.max(maxEndingHere+A[i],A[i]);
-    	maxSoFar=Math.max(maxSoFar, maxEndingHere);	
-    }
-    return maxSoFar;
-}
+        return globalMax
