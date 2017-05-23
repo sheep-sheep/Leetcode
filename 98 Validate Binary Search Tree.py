d09cdcaf70b0b739783aefadb47145b1351a2169
Yang <sheep-sheep@users.noreply.github.com>
2017-05-22 23:16:27 -0400

    Update 98 Validate Binary Search Tree.py

--- 98 Validate Binary Search Tree.py
+++ 98 Validate Binary Search Tree.py
@@ -19,3 +19,9 @@ class Solution(object):
         self.inOrder(root.left, output)
         output.append(root.val)
         self.inOrder(root.right, output)
+        
+        
+# The reason to use a MIN and MAX is to help pass the previous min/max value to next level.
+# If we don't have such infomation the BST order can't be valid.
+
+# python max/min number: float('inf')/float('-inf')
