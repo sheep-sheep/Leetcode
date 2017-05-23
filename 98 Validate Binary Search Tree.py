06908ef9f51c787326b06f6421cd9cda1cc2f64a
Yang <sheep-sheep@users.noreply.github.com>
2017-05-22 22:55:53 -0400

    Create 98 Validate Binary Search Tree.py

---
+++ 98 Validate Binary Search Tree.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        output = []
+        self.inOrder(root, output)
+        
+        for i in range(1, len(output)):
+            if output[i-1] >= output[i]:
+                return False
+        return True
+
+    def inOrder(self, root, output):
+        if root is None:
+            return
+        
+        self.inOrder(root.left, output)
+        output.append(root.val)
+        self.inOrder(root.right, output)
