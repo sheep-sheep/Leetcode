3e0d831d87833f19744e1e890c80debae4e9c2fe
Yang <sheep-sheep@users.noreply.github.com>
2017-06-13 23:18:40 -0400

    Create Summary_BST.py

--- Summary_BST.py
+++ Summary_BST.py
@@ -1,4 +1,4 @@
-BST Basic Operations:
+1. BST Basic Operations:
 
 # Insert Recursive
 def insert(curr, parent, val):
@@ -75,3 +75,58 @@ def find_stack(root, val):
                 return root
         else:
             return None
+
+# delete
+# 1. only need to delete leaf, either right or left
+# 2. need to delete non-leaf node, with one child
+# 3. need to delete non-leaf node, with 2 children, will need to 
+# find sucessor(min in right child), make sure the sucessor's right child is relocated.
+def delete(root, val):
+    def find(root, parent, val):
+        if root:
+            if val < root.val:
+                return find(root.left, root, val)
+            elif val > root.val:
+                return find(root.right, root, val)
+            else:
+                return root, parent
+        return None
+    def isLeftChild(root, parent):
+        return parent.left == root
+    def isleaf(root):
+        return (root.right is None) and (root.left is None)
+    def getSucessorTree(root):
+        curr = root
+        while(True):
+            parent = curr
+            curr = curr.left
+            if curr.left is None:
+                break
+        sucessor = curr
+        if sucessor.right:
+            parent.left = sucessor.right
+        sucessor.right = root
+        return sucessor
+
+    curr, parent = find(root, root, val)
+    if not curr: return
+    # case 1
+    if isleaf(curr):
+        if isLeftChild(curr, parent):
+            parent.left = None
+        else:
+            parent.right = None
+    # case 3
+    elif (curr.left and curr.right):
+        sucessor = getSucessorTree(curr.right)
+        if isLeftChild(curr, parent):
+            parent.left = sucessor
+        else:
+            parent.right = sucessor
+    # case 2
+    else:
+        if isLeftChild(curr, parent):
+            parent.left = curr.right if curr.right else curr.left
+        else:
+            parent.right = curr.right if curr.right else curr.left
+    return root
