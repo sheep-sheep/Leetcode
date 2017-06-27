c5aeba807035ee1e7f1b74ba4aed6f2e3f86aae8
Yang <sheep-sheep@users.noreply.github.com>
2017-06-26 23:55:31 -0400

    this is wrong answer

--- 79 Word Search.py
+++ 79 Word Search.py
@@ -1,28 +1,28 @@
-class Solution(object):
-    def exist(self, board, word):
-        """
-        :type board: List[List[str]]
-        :type word: str
-        :rtype: bool
-        """
-        def exist(board, used, i, j, part):
-            if len(part)==0: return True
-            if j+1<len(board[0]) or j-1>=0 or i+1<len(board) or i-1>=0 : return False
-            if board[i][j] != part[0] or used[i][j]: return False
-            used[i][j] = True
-            return exist(board, used, i, j+1, part[1:])or \
-                   exist(board, used, i, j-1, part[1:])or \
-                   exist(board, used, i+1, j, part[1:])or \
-                   exist(board, used, i-1, j, part[1:])
+# class Solution(object):
+#     def exist(self, board, word):
+#         """
+#         :type board: List[List[str]]
+#         :type word: str
+#         :rtype: bool
+#         """
+#         def exist(board, used, i, j, part):
+#             if len(part)==0: return True
+#             if j+1<len(board[0]) or j-1>=0 or i+1<len(board) or i-1>=0 : return False
+#             if board[i][j] != part[0] or used[i][j]: return False
+#             used[i][j] = True
+#             return exist(board, used, i, j+1, part[1:])or \
+#                    exist(board, used, i, j-1, part[1:])or \
+#                    exist(board, used, i+1, j, part[1:])or \
+#                    exist(board, used, i-1, j, part[1:])
 
 
-        rowN = len(board)
-        colN = len(board[0])
-        used = [[False]*colN for _ in range(rowN)]
-        for i in range(rowN):
-            for j in range(colN):
-                if exist(board, used, i, j, word): return True
-        return False
+#         rowN = len(board)
+#         colN = len(board[0])
+#         used = [[False]*colN for _ in range(rowN)]
+#         for i in range(rowN):
+#             for j in range(colN):
+#                 if exist(board, used, i, j, word): return True
+#         return False
     
 # Use one board but need to change it and assign the value back
 # Use ^256 to operate on char 
