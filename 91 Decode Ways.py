6c481ce96ae44516ba92e47dafb9f60c229eeb02
Yang <sheep-sheep@users.noreply.github.com>
2017-07-18 22:35:01 -0400

    Update 91 Decode Ways.py

--- 91 Decode Ways.py
+++ 91 Decode Ways.py
@@ -1,20 +1,13 @@
 class Solution(object):
     def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if len(s)==0:
+        #dp[i] = dp[i-1] if s[i] != "0"
+        #       +dp[i-2] if "09" < s[i-1:i+1] < "27"
+        if s == '':
             return 0
-        dp = [1] * len(s)
-        if len(s)==1:
-            if s[0]=='0':
-                return 0
-            else:
-                return 1
-        for i in range(1, len(s) + 1):
-            if s[i-1] == '0':
-                dp[i] = dp[i-2]
-            else:
-                dp[i] = dp[i - 1] if int(s[i - 1:i + 1]) > 26 else dp[i - 1] + dp[i - 2]
+        dp = [1] + [0] * (len(s))
+        for i in range(1, len(s)+1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            if i != 1 and s[i-2:i] < "27" and s[i-2:i] > "09":  #"01"ways = 0
+                dp[i] += dp[i-2]
         return dp[-1]
