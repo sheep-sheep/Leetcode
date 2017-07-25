94e9d015d30c130a7585c849ce5a71e92880f64c
Yang <sheep-sheep@users.noreply.github.com>
2017-07-24 23:20:26 -0400

    Update 84 Largest Rectangle in Histogram.py

--- 84 Largest Rectangle in Histogram.py
+++ 84 Largest Rectangle in Histogram.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        dp = [(1,0)]+[(1,i) for i in heights]
-        tmp = (1, 0)
+        dp = [((0,1), 0)]+[((i,i+1), heights[i-1]) for i in range(1, len(heights))]
+
         for i in range(1, len(heights)+1):
-            if heights[i-1]==0:
-                tmp = dp[i-1]
-                continue
-            newhist = (dp[i][0]+1)*min(dp[i-1][1], heights[i-1])
-            oldhist = dp[i-1][0]*dp[i-1][1]
+            newhist = (dp[i-1][0][1]+1-dp[i-1][0][0])*min(dp[i-1][1], heights[i-1])
+            oldhist = (dp[i-1][0][1]-dp[i-1][0][0])*dp[i-1][1]
             hist = heights[i-1]
+
             if newhist>=hist:
                 if newhist>oldhist:
-                    dp[i] = (dp[i-1][0]+1, min(dp[i-1][1], heights[i-1]))
+                    dp[i] = ((dp[i][0][1]+1-dp[i][0][0]), min(dp[i-1][1], heights[i-1]))
                 else:
                     dp[i] = (dp[i - 1][0], dp[i - 1][1])
             elif newhist<oldhist:
