f138c0472f461bbfe37e9f2762f4f1a3c4336ecd
Yang <sheep-sheep@users.noreply.github.com>
2017-07-31 23:29:55 -0400

    so sad i have to go to bed now

--- 84 Largest Rectangle in Histogram.py
+++ 84 Largest Rectangle in Histogram.py
@@ -4,20 +4,38 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        dp = [((0,1), 0)]+[((i,i+1), heights[i-1]) for i in range(1, len(heights))]
-
-        for i in range(1, len(heights)+1):
-            newhist = (dp[i-1][0][1]+1-dp[i-1][0][0])*min(dp[i-1][1], heights[i-1])
-            oldhist = (dp[i-1][0][1]-dp[i-1][0][0])*dp[i-1][1]
-            hist = heights[i-1]
-
-            if newhist>=hist:
-                if newhist>oldhist:
-                    dp[i] = ((dp[i][0][1]+1-dp[i][0][0]), min(dp[i-1][1], heights[i-1]))
+        def maxMidArea(heights, left, mid, right):
+            i = mid
+            j = mid+1
+            area = 0
+            h = min(heights[i], heights[j]);
+            while( i >= left and j <= right):
+                h = min(h, heights[i], heights[j])
+                area = max(area, (j-i+1)*h)
+                if i == left:
+                    j+=1
+                elif j == right:
+                    i-=1
                 else:
-                    dp[i] = (dp[i - 1][0], dp[i - 1][1])
-            elif newhist<oldhist:
-                dp[i] = (dp[i-1][0], dp[i-1][1])
-            else:
-                dp[i] = (1, heights[i-1])
-        return max(dp[-1][0]*dp[-1][1], tmp[0]*tmp[1])
+                    # if left and right haven't reached boundary,
+                    # choose the largest height side which will be
+                    # more likely increase the area.
+                    if heights[i-1] > heights[j+1]:
+                        i -= 1
+                    else:
+                        j += 1
+            return area
+
+        def maxArea(heights, left, right):
+            if left == right:
+                return heights[left]*1
+
+            mid = left + (right-left)/2
+            leftArea = maxArea(heights, left, mid)
+            rightArea = maxArea(heights, mid+1, right)
+            midArea = maxMidArea(heights, left, mid, right)
+            return max(leftArea, rightArea, midArea)
+
+        if not heights:
+            return 0
+        return maxArea(heights, 0, len(heights)-1) #Typical binary search/divide and conquer
