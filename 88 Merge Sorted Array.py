d76b2365c64d58a8bbbdc28d631a9f8859168b10
Yang <sheep-sheep@users.noreply.github.com>
2017-08-02 23:14:46 -0400

    Update 88 Merge Sorted Array.py

--- 88 Merge Sorted Array.py
+++ 88 Merge Sorted Array.py
@@ -24,3 +24,14 @@ class Solution(object):
                 
                 
 # wow, i can't believe i can solove them so quickly!                
+
+def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
