7d405d5387d096f7ab680a8af2f3e563fb157c53
Yang <sheep-sheep@users.noreply.github.com>
2017-08-23 22:34:01 -0400

    Update 516 Longest Palindromic Subsequence.py

--- 516 Longest Palindromic Subsequence.py
+++ 516 Longest Palindromic Subsequence.py
@@ -6,10 +6,16 @@ class Solution(object):
         """
         N = len(s)
         dp = [[1 if i == j else 0 for i in range(N + 1)] for j in range(N + 1)]
-        for i in range(N+1, 0, -1):
+        if s == s[::-1]:
+            return len(s)
+        for i in range(N, 0, -1):
             # The reason it needs backwards is dp[i+1] is requiring the next position's result.
             # if we go from bottom, all the values will be unknown.
             for j in range(i+1, N+1):
                 if i < j:
                     dp[i][j] = dp[i+1][j-1] + 2 if (s[i - 1] == s[j - 1]) else max(dp[i+1][j], dp[i][j-1])
         return dp[1][N]
+    
+    
+# check if the string is palindrome is the key, i was using set(), instead we should follow the definition.
+# the check we save us lots of time for the real problem.
