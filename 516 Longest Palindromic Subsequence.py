559677e9d715faff6235e1919b42e2f647d9d601
Yang <sheep-sheep@users.noreply.github.com>
2017-08-23 21:52:47 -0400

    Update 516 Longest Palindromic Subsequence.py

--- 516 Longest Palindromic Subsequence.py
+++ 516 Longest Palindromic Subsequence.py
@@ -6,11 +6,10 @@ class Solution(object):
         """
         N = len(s)
         dp = [[1 if i == j else 0 for i in range(N + 1)] for j in range(N + 1)]
-
-        for i in range(0, N):
-            for j in range(i, N+1):
-                if i != j:
-                    dp[i][j] = dp[i+1][j-1] + 2 if (s[i - 1] == s[j - 1]) else max(dp[i-1][j], dp[i][j+1])
-        return dp[N][1]
-
-print Solution().longestPalindromeSubseq("bbbab")
+        for i in range(N+1, 0, -1):
+            # The reason it needs backwards is dp[i+1] is requiring the next position's result.
+            # if we go from bottom, all the values will be unknown.
+            for j in range(i+1, N+1):
+                if i < j:
+                    dp[i][j] = dp[i+1][j-1] + 2 if (s[i - 1] == s[j - 1]) else max(dp[i+1][j], dp[i][j-1])
+        return dp[1][N]
