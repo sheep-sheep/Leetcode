888fc50326d4e07953179fd056462572a8c57bec
Yang <sheep-sheep@users.noreply.github.com>
2017-09-08 23:45:47 -0400

    Update 46 Permutations.py

--- 46 Permutations.py
+++ 46 Permutations.py
@@ -52,3 +52,26 @@ class Solution(object):
             return recursivePermute(nums[0], nums[1:])
         else:
             return [nums] if nums else []
+        
+        
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums)<2:
+            return nums
+        
+        res = []
+        for (idx,num) in enumerate(nums):
+            for item in self.permute(nums[:idx]+nums[idx+1:]):
+            	print idx, nums[:idx]+nums[idx+1:]
+                tmp = [num]
+                tmp.extend(item)
+                res.append(tmp)
+                print res
+        
+        return res
+
+print Solution() .permute([1,2])        
