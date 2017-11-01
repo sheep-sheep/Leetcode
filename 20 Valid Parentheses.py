a9b62e89c69d47dfc73ac4f2aba56753c5ea1324
Yang <sheep-sheep@users.noreply.github.com>
2017-10-31 23:41:32 -0400

    aced

--- 20 Valid Parentheses.py
+++ 20 Valid Parentheses.py
@@ -28,3 +28,24 @@ class Solution(object):
                     else:
                         return False
         return holder_0 == [] and holder_1 == []
+
+# Damn, why did i use such dump method?
+# check my current implementation, i truely learnt sth right?
+
+class Solution(object):
+    def isValid(self, s):
+        if s == '':
+            return False
+        
+        stack = []
+        for char in s:
+            if char in ['(', '{', '[']:
+                stack.append(char)
+            else:
+                if stack:
+                    last = stack.pop()
+                    if (last == '[' and char != ']') or (last == '(' and char != ')') or (last == '{' and char != '}'):
+                        return False
+                else:
+                    return False
+        return len(stack)==0
