6c937a5a50b61d97152eaebfb0d232376f06d06d
Yang <sheep-sheep@users.noreply.github.com>
2017-11-29 19:14:19 -0500

    i was so dumb 7 months ago

--- 75 Sort Colors.py
+++ 75 Sort Colors.py
@@ -16,3 +16,17 @@ class Solution(object):
                 nums[i] = 0
                 i += 1
                 
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        red, blue = 0, -1
+        for i in range(len(nums)):
+            while nums[i] == 2 and i < len(nums)+blue:
+                nums[blue], nums[i] = nums[i], nums[blue]
+                blue -= 1
+            while nums[i] == 0 and i > red:
+                nums[red], nums[i] = nums[i], nums[red]
+                red += 1
