b52ea2b22e1af56ef619df0c718e36e2820545a6
Yang <sheep-sheep@users.noreply.github.com>
2017-12-26 21:29:18 -0500

    Update 42 Trapping Rain Water.py

--- 42 Trapping Rain Water.py
+++ 42 Trapping Rain Water.py
@@ -1,16 +1,17 @@
 class Solution(object):
+    # Solution 1, it's TLE becuase of O(n^2)
     def trap(self, height):
         """
         :type height: List[int]
         :rtype: int
         """
-        waterLevel = []
-        left = 0
-        for h in height:
-            left = max(left, h) 
-            waterLevel += [left] # over-fill it to left max height
-        right = 0
-        for i, h in reversed(list(enumerate(height))):
-            right = max(right, h)
-            waterLevel[i] = min(waterLevel[i], right) - h # drain to the right height
-        return sum(waterLevel)
+        res = 0
+        for (i, h) in enumerate(height):
+            max_left = max_right = 0
+            for j in range(i, -1, -1):
+                max_left = max(max_left, height[j])
+            for j in range(i, len(height)):
+                max_right = max(max_right, height[j])
+            res += min(max_left, max_right) - h
+
+        return res
